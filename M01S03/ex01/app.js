04f47fe9f3caf173ce5b485961d762451079c004
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2020-10-08 22:05:21 +0300

    Curs 03

---
+++ M01S03/ex01/app.js
@@ -0,0 +1,16 @@
+console.log(object.age);
+console.log(object.surname);
+
+console.log(
+  ' Ma numesc ' +
+    object.name +
+    '' +
+    object.surname +
+    'si am ' +
+    object.age +
+    ' ani ',
+);
+
+// Afiseaza anul in care te-ai nascut
+// scazand varsta din anul curent
+console.log(2020 - object.age / 2);
