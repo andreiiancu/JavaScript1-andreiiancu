6cc505a52172cbae79476a5178d25161f483da8c
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2020-10-22 00:23:56 +0300

    Tema 20 octombrie

--- M01S04/ex01/app.js
+++ M01S04/ex01/app.js
@@ -24,3 +24,32 @@ if (value % 2 === 0) {
 var message = `Valoarea este ${value} si este ${word}`;
 messageParagraph.innerText = message;
 //messageParagraph.textContent = message- exista aceasta posibilitate
+
+// Folosind metoda prompt() afiseaza in pagina mesajul: “Numarul
+// introdus este mai mic | mai mare decat 20.”
+// NU AM REUSIT
+
+// Foloseste aceeasi metoda pentru a captura numele
+// utilizatorului si pentru a afisa in
+// document mesajul: “Numele meu este: xxx yyy.”
+// var response = prompt('What is your name?');
+
+// alert('Numele meu este ' + response);
+
+// Folosind aceeasi metoda afiseaza in document propozitia:
+// “Numele introdus are xxx caractere.” Inlocuieste
+// spatiile cu stringul gol pentru a nu le numara.
+// var response = prompt('What is your name?');
+
+// alert('Numele meu are ' + ' ' 'caractere.');
+
+// Folosind prompt() si String.includes()
+// afiseaza mesajul: “Numele introdus contine|nu contine litera a.”
+// var response = prompt('What is your name?');
+
+// if (prompt.resonse.includes('a')) {
+//   alert('Numele introdus contine litera a.');
+// } else {
+//   alert('Numele introdus nu contine litera a.');
+// }
+// Chiar daca are litera a, zice ca nu contine litera a!!!
--- M01S04/ex04/app.js
+++ M01S04/ex04/app.js
@@ -16,6 +16,55 @@ var i;
 //   console.log(i);
 // }
 
+// for (i = 1; i <= 100; i++) {
+//   if (i % 5 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// Modifica exemplul astfel incat bucla sa numere de la 1 la 100.
+// for (i = 1; i <= 100; i++) {
+//   if (i > 100) {
+//     break;
+//   }
+
+//   console.log(i);
+// }
+
+// Modifica exemplul astfel incat bucla sa numere de la 1
+// la 99 (dar nu schimba numarul 100)=<100.
+// for (i = 1; i <= 100; i++) {
+//   if (i >= 100) {
+//     break;
+//   }
+
+//   console.log(i);
+// }
+
+// Folosind keywordul break opreste bucla atunci cand
+// numarul este egal cu 50 (dar afiseaza-l).= if (i === 50) {   break; }
+// for (i = 1; i <= 100; i++) {
+//   if (i > 50) {
+//     break;
+//   }
+
+//   console.log(i);
+// }
+
+// Folosind keywordul continue afiseaza doar numerele
+// pare intre 1 si 100 (inclusiv 100).
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// In mod similar, afiseaza doar numerele care sunt divizibile cu 5.
+// !-negare
 for (i = 1; i <= 100; i++) {
   if (i % 5 !== 0) {
     continue;
@@ -23,13 +72,3 @@ for (i = 1; i <= 100; i++) {
 
   console.log(i);
 }
-
-// Modifica exemplul astfel incat bucla sa numere de la 1 la 100.
-// Modifica exemplul astfel incat bucla sa numere de la 1
-// la 99 (dar nu schimba numarul 100)=<100.
-// Folosind keywordul break opreste bucla atunci cand
-// numarul este egal cu 50 (dar afiseaza-l).= if (i === 50) {   break; }
-// Folosind keywordul continue afiseaza doar numerele
-// pare intre 1 si 100 (inclusiv 100).
-// In mod similar, afiseaza doar numerele care sunt divizibile cu 5.
-// !-negare
