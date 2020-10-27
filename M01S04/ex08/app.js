6d72990aaeb0c657bf1098e4835081d2850b06dd
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2020-10-27 17:02:47 +0200

    Ex /Curs04

--- M01S04/ex08/app.js
+++ M01S04/ex08/app.js
@@ -23,13 +23,57 @@ let i = 1;
 
 // Folosind keywordul break opreste bucla atunci
 // cand numarul este egal cu 50 (dar afiseaza-l).
-while (i < 100) {
-  console.log(i);
+// while (i < 100) {
+//   console.log(i);
 
-  if (i === 50) {
-    break;
-  }
+//   if (i === 50) {
+//     break;
+//   }
 
-  i++;
-  //i=i+1;
-}
+//   i++;
+//   //i=i+1;
+// }
+
+// Modifica exemplul astfel incat bucla sa
+// numere de la 1 la 67.
+// while (i < 100) {
+//   console.log(i);
+
+//   if (i === 67) {
+//     break;
+//   }
+
+//   i++;
+//   //i=i+1;
+// }
+
+// Modifica exemplul astfel incat bucla sa numere de la 1 la 66.
+// while (i < 67) {
+//   console.log(i);
+
+//   i++;
+//   //i=i+1;
+// }
+
+// Folosind keywordul break opreste bucla atunci cand numarul este egal cu 12 (dar afiseaza-l).
+// while (i < 100) {
+//   console.log(i);
+
+//   if (i === 12) {
+//     break;
+//   }
+
+//   i++;
+//   //i=i+1;
+// }
+
+// Folosind keywordul continue afiseaza doar numerele pare intre 1 si 32 (inclusiv).
+// for (i = 1; i <= 32; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// Folosind metoda prompt de doua ori cere utilizatorului un numar si o limita superioara apoi afiseaza toti multiplii de numar intre 5 si limita superioara introduse.
--- M01S04/ex09/app.js
+++ M01S04/ex09/app.js
@@ -36,25 +36,56 @@ let i = 1;
 //TEMA:
 // Modifica exemplul astfel incat bucla sa
 // numere de la 1 la 52
-do {
-  console.log(i);
+// do {
+//   console.log(i);
 
-  if (i === 50) {
-    break;
-  }
+//   if (i === 52) {
+//     break;
+//   }
 
-  i++;
-} while (i < 100);
+//   i++;
+// } while (i < 100);
 
 // Modifica exemplul astfel incat bucla sa
 // numere de la 1 la 51.
+// do {
+//   console.log(i);
+
+//   if (i === 51) {
+//     break;
+//   }
+
+//   i++;
+// } while (i < 100);
+
 // Folosind keywordul break opreste bucla atunci
 // cand numarul este egal cu 12 (dar afiseaza-l)
+// do {
+//   console.log(i);
+
+//   if (i === 12) {
+//     break;
+//   }
+
+//   i++;
+// } while (i < 100);
+
 // Folosind keywordul continue afiseaza doar
-// numerele impare intre 8 si 32.
+// numerele pare intre 8 si 32.
+// for (i = 8; i <= 32; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
 
 // Folosind metoda prompt de trei ori cere
 // utilizatorului un numar, o limita inferioara
 // si o limita superioara apoi afiseaza toti
 // multiplii de numar intre limita inferioara si
 // limita superioara introduse.
+
+var number = prompt('Introdu un numar');
+var value = prompt('Introdu o limita Inferioara');
+var value1 = prompt('Introdu o limita superioara');
